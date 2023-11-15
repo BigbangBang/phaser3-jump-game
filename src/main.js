16382c36383c8215c6b962094079e0748abe6f58
LorenWang <0.BigbigBang@gmail.com>
2023-11-15 20:14:47 +0800

    set up project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Phaser from '../phaser/lib.js'
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+})
+// console.dir(Phaser);
+// console.log('hello world');
