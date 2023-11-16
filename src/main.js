acd9f87edca363563dd7d38097b8225153a39f42
LorenWang <0.BigbigBang@gmail.com>
2023-11-16 13:30:19 +0800

    load background

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import Phaser from '../phaser/lib.js'
+import Phaser from '../phaser/lib.js';
+import Game from './scenes/game.js';
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 480,
     height: 640,
+    scene: [Game],
 })
 // console.dir(Phaser);
 // console.log('hello world');
