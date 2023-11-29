b508a2dda585c01e775f1fc81f8ac1b70cf85305
LorenWang <0.BigbigBang@gmail.com>
2023-11-29 13:40:09 +0800

    finish

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import Phaser from '../phaser/lib.js';
 import Game from './scenes/game.js';
+import GameOver from './scenes/gameOver.js';
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 480,
     height: 640,
-    scene: [Game],
+    scene: [Game, GameOver],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 200
+            }
+        },
+        debug: true
+    }
 })
 // console.dir(Phaser);
 // console.log('hello world');
